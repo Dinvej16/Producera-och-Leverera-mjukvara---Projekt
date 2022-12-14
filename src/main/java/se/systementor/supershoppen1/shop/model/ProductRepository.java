18c1a9d3dce8cd54c43a67583e3fb8c53a61243e
Isabella <90781622+S3she@users.noreply.github.com>
2022-12-14 11:16:17 +0100

    Updated Subscriber

--- src/main/java/se/systementor/supershoppen1/shop/model/ProductRepository.java
+++ src/main/java/se/systementor/supershoppen1/shop/model/ProductRepository.java
@@ -3,5 +3,7 @@ package se.systementor.supershoppen1.shop.model;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-    
+
+
+    //ta bort crud
 }
