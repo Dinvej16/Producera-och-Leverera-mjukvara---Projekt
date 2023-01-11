e48a0424ef552f7b0203339c3f2ce2cc1c706ff3
Carl-Lander <92018685+Carl-Lander@users.noreply.github.com>
2023-01-11 16:23:44 +0100

    updated css

--- src/main/java/se/systementor/supershoppen1/shop/controller/ProductController.java
+++ src/main/java/se/systementor/supershoppen1/shop/controller/ProductController.java
@@ -1,32 +1,38 @@
 package se.systementor.supershoppen1.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-    public class ProductController {
+public class ProductController {
 
-        public double calculateDiscount(double totalOrderAmount, String productCategory) {
-            if (productCategory.equals("Beverages")) {
-                if (totalOrderAmount < 1000) {
-                    return 0;
-                } else if (totalOrderAmount >= 1000 && totalOrderAmount < 5000) {
-                    return 0.02;
-                } else if (totalOrderAmount >= 5000 && totalOrderAmount < 15000) {
-                    return 0.03;
-                } else {
-                    return 0.04;
-                }
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public double calculateDiscount(@RequestParam("totalOrderAmount") double totalOrderAmount,
+                                    @RequestParam("productCategory") String productCategory) {
+        if (productCategory.equals("Beverages")) {
+            if (totalOrderAmount < 1000) {
+                return 0;
+            } else if (totalOrderAmount >= 1000 && totalOrderAmount < 5000) {
+                return 0.02;
+            } else if (totalOrderAmount >= 5000 && totalOrderAmount < 15000) {
+                return 0.03;
             } else {
-                if (totalOrderAmount < 1000) {
-                    return 0;
-                } else if (totalOrderAmount >= 1000 && totalOrderAmount < 5000) {
-                    return 0.01;
-                } else if (totalOrderAmount >= 5000 && totalOrderAmount < 15000) {
-                    return 0.02;
-                } else {
-                    return 0.03;
-                }
+                return 0.04;
+            }
+        } else {
+            if (totalOrderAmount < 1000) {
+                return 0;
+            } else if (totalOrderAmount >= 1000 && totalOrderAmount < 5000) {
+                return 0.01;
+            } else if (totalOrderAmount >= 5000 && totalOrderAmount < 15000) {
+                return 0.02;
+            } else {
+                return 0.03;
             }
         }
     }
+}
+
 
